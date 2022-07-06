5478639ae8de26f7930aa33510e24e0a6dc1227f
Asim Ali DM <100372138+asimdm@users.noreply.github.com>
2022-07-06 16:38:53 +0530

    Add files via upload

---
+++ code/simple_print.py
@@ -0,0 +1,4 @@
+name='John Smith'
+age=20
+is_new=True
+print("{0} is {1} years old and it is {2}, he is a new patient.".format(name,age,is_new))
---
+++ code/sum.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter second number:"))
+c=a+b
+print("The sum of the numbers entered is {} ".format(c))
+c=a-b
+print("The difference of the numbers entered is {} ".format(c))
+c=a*b
+print("The multiplication of the numbers entered is {} ".format(c))
+c=a/b
+print("The division of the numbers entered is {} ".format(c))
+c=a%b
+print("The modulus of the numbers entered is {} ".format(c))
